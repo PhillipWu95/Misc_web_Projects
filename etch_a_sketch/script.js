c0d53291aa8407dfa83a46a2ac2f9bf4b84517f8
Phillip <phillipwu95@gmail.com>
2019-09-09 01:04:23 +1000

    added button and functions

--- etch_a_sketch/script.js
+++ etch_a_sketch/script.js
@@ -1,33 +1,113 @@
 const mainContent = document.querySelector(".mainContent");
 let gridSize = 16;
-
 let drawingPad = document.createElement("div");
+let buttonClicked = "new";
 mainContent.appendChild(drawingPad);
-let drawingPadAttr = 
-"display: grid;"
-+ "background-color: black;"
-+ "grid-template-columns: repeat(" + gridSize + ",1fr);"
-+ "grid-template-rows: repeat(" + gridSize + ",1fr);"
-+ "text-align: center;"
-+ "margin: 0 auto;"
-+ "width: 500px;"
-+ "height: 500px;"
-+ "border: solid 2px red;";
 
-drawingPad.setAttribute("style", drawingPadAttr);
+function setGridSize(component, gridSize=16, backgroundColor="black") {
+    let drawingPadAttr = 
+        "display: grid;"
+        + "background-color: " + backgroundColor + ";"
+        + "grid-template-columns: repeat(" + gridSize + ",1fr);"
+        + "grid-template-rows: repeat(" + gridSize + ",1fr);"
+        + "text-align: center;"
+        + "margin: 0 auto;"
+        + "width: 969px;"
+        + "height: 960px;"
+        + "border: solid 2px red;";
 
-function gridComponentEvent(component, color="white") {
+        component.setAttribute("style", drawingPadAttr);
+        initElements(gridSize);
+        let allElements=document.querySelectorAll(".gridComponent");
+        return allElements;
+}
+
+function initDrawingPad() {
+    allElements =  setGridSize(drawingPad);
+    return allElements;
+}
+
+
+allGridElements = initDrawingPad();
+// allGridElements.forEach(element => {
+//     console.log(element);
+// });
+
+
+// let drawingPadAttr = 
+// "display: grid;"
+// + "background-color: black;"
+// + "grid-template-columns: repeat(" + gridSize + ",1fr);"
+// + "grid-template-rows: repeat(" + gridSize + ",1fr);"
+// + "text-align: center;"
+// + "margin: 0 auto;"
+// + "width: 969px;"
+// + "height: 960px;"
+// + "border: solid 2px red;";
+
+// drawingPad.setAttribute("style", drawingPadAttr);
+
+function setDrawingColor(component, color="white") {
     component.setAttribute("style", "background-color: " + color + ";");
 }
 
-var i;
-var j;
-for(i = 0; i < gridSize; i++) {
-    for(j = 0; j < gridSize; j++) {
-        let gridComponent = document.createElement("div");
-        drawingPad.appendChild(gridComponent);
-        gridComponentAttr = ""
-            + "grid-area: " + i+1 + "/" + j+1 + "/" + i+2 + "/" + j+2 + ";";
-        gridComponent.addEventListener("mouseover", () => {gridComponentEvent(gridComponent);});
+function removeSetColor(component) {
+    component.removeEventListener("mouseover", setColor);
+}
+
+function initElements(gridSize) {
+    let i;
+    let j;
+    for(i = 0; i < gridSize; i++) {
+        for(j = 0; j < gridSize; j++) {
+            let gridComponent = document.createElement("div");
+            drawingPad.appendChild(gridComponent);
+            gridComponentAttr = ""
+                + "grid-area: " + i+1 + "/" + j+1 + "/" + i+2 + "/" + j+2 + ";";
+            gridComponent.classList.add("gridComponent");
+            gridComponent.addEventListener("mouseover", function setColor() {
+                    setDrawingColor(gridComponent);   
+                    removeSetColor(gridComponent);
+                }
+            );
+            
+        }
     }
 }
+
+
+function randomColor() {
+    let ran255 = () => {
+        return Math.floor(255 * Math.random());
+    }
+    // console.log(ran255());
+    let rgb = ""
+            + "rgb(" + ran255()
+            + ","    + ran255()
+            + ","    + ran255()
+            + ")";
+    return rgb;
+}
+
+const colorBtn = document.querySelector("#color");
+const rainbowBtn = document.querySelector("#rainbow");
+const newBtn = document.querySelector("#new");
+
+
+rainbowBtn.addEventListener('click', function rainbowBtnEvent() {
+        allGridElements.forEach((gridComponent) => {
+            gridComponent.addEventListener("mouseover", function setColor() {
+                setDrawingColor(gridComponent, randomColor());
+                gridComponent.removeEventListener("mouseover", setColor);
+            });
+        });
+    }
+);
+
+newBtn.addEventListener('click', function newBtnEvent() {
+    let gridSize = parseInt(prompt("Enter grid size: "));
+    while(isNaN(gridSize) || gridSize < 1) {
+        gridSize = prompt("Enter grid size: (in positive number please)");
+    }
+    allGridElements =  setGridSize(drawingPad, gridSize);
+});
